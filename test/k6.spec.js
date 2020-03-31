32cddba6fe339be1f42a6785f3468cecf0dae552
Alejandro <morsisdivine@gmail.com>
2020-04-01 09:58:50 +1100

    add local and ext test

--- test/k6.spec.js
+++ test/k6.spec.js
@@ -2,9 +2,10 @@ import { group, check } from "k6";
 import http from "k6/http";
 
 export default function() {
+
   group('villain check', function() {
     group('check villain', function() {
-      let res = http.get("http://localhost:3000/villain");
+      let res = http.get(`${__ENV.MY_HOSTNAME}/villain`);
       let bJSNO = JSON.parse(res.body);
     
       check(res, {
@@ -15,7 +16,7 @@ export default function() {
       });
       });
     group('check villain image 1', function() {
-      let res2 = http.get("http://localhost:3000/img/covid19_1.jpg");
+      let res2 = http.get(`${__ENV.MY_HOSTNAME}/img/covid19_1.jpg`);
 
       check(res2, {
         "image status 200": r => r.status === 200,
@@ -23,7 +24,7 @@ export default function() {
       });
     });
     group('check villain image 2', function() {
-      let res3 = http.get("http://localhost:3000/img/covid19_2.jpg");
+      let res3 = http.get(`${__ENV.MY_HOSTNAME}/img/covid19_2.jpg`);
       console.log(res3.body.length);
       check(res3, {
         "image status 200": r => r.status === 200,
