6a1b651e7153d3265bec4e738f82b1074e2936e4
Alejandro <morsisdivine@gmail.com>
2020-04-03 09:44:36 +1100

    images and features (#2)
    * images and features
    
    * remove DS_Store
    
    * update gitignore
    
    * remove DS_Store
    
    * update gitignore
    
    * add local and ext test
    
    * fix path

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const path = require('path');
 
 var app = express();
 // Default Port 3000 unless specified at start
@@ -9,10 +10,26 @@ app.listen(port, () => {
 });
 
 app.get("/villain", (req, res, next) => {
-    res.json([{
-        id: 1,
-        type: 'covid19',
-        displayName: 'CORONA',
-        img: 'covid19.jpg'
-    }]);
+    console.log('Returning villain list');
+    res.json([
+        {
+            "id": 1,
+            "type": "covid19",
+            "displayName": "CORONA",
+            "images": [
+                {
+                    "id": 1,
+                    "name": "covid19_1.jpg",
+                    "credit": "https://unsplash.com/@cdc"
+                },
+                {
+                    "id": 2,
+                    "name": "covid19_2.jpg",
+                    "credit": "https://unsplash.com/@cdc"
+                },
+            ]
+        }
+    ]);
    });
+
+app.use('/img', express.static(path.join(__dirname,'img')));
---
+++ idea/IDEA.md
@@ -0,0 +1,17 @@
+# IDEA
+
+What are the attributes of a villain
+
+- summary
+  - id
+  - name
+  - image(s)
+- images
+  - id
+  - image
+  - credit
+
+## PATHS
+
+GET /villain
+GET /img/**
