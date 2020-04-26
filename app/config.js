a240b448a128582cbe346fd235e5f41d53e94297
Alejandro <morsisdivine@gmail.com>
2020-04-27 08:59:10 +1000

    create DB for testing

--- app/config.js
+++ app/config.js
@@ -3,12 +3,19 @@ require('dotenv').config()
 const { Pool } = require('pg')
 
 const isProduction = process.env.NODE_ENV === 'production'
+const isCi = process.env.ISCI
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
-const pool = new Pool({
-  connectionString: isProduction ? process.env.ELEPHANT_URL : connectionString,
-  ssl: isProduction,
-})
-
+if (isCi) {
+    const pool = new Pool({
+        connectionString: isCi ? process.env.ELEPHANT_URL : connectionString,
+        ssl: false,
+        })
+} else {
+    const pool = new Pool({
+    connectionString: isProduction ? process.env.ELEPHANT_URL : connectionString,
+    ssl: isProduction,
+    })
+}
 module.exports = { pool }
