e9b6991bbdd23009d9c632945763c5746029fc4c
Alejandro <morsisdivine@gmail.com>
2020-04-29 22:33:56 +1000

    Update user (#7)
    * added PUT and also test and perf test
    
    * add k6 back
    
    * run pointing to perf DB
    
    * fix db connection
    
    * k6 quiet

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser')
-const { pool } = require('./config')
 const cors = require('cors')
+let controllers = require('./controller/sql');
 
 let swaggerUi = require('swagger-ui-express'),
 swaggerDocument = require('./swagger/openapi.json');
@@ -20,37 +20,15 @@ app.use(cors())
 // The below routes are for swagger documentation
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// TODO: move the below into a separate V1 controller *********************************************
-const getUser = (request, response) => {
-    pool.query('SELECT * FROM leaderboard ORDER BY score DESC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-      console.log('user list loaded successfully')
-    })
-  }
-  
-  const addUser = (request, response) => {
-    const { username, score } = request.body
-  
-    pool.query('INSERT INTO leaderboard (username, score) VALUES ($1, $2)', [username, score], error => {
-      if (error) {
-        throw error
-      }
-      response.status(201).json({ status: 'success', message: 'User added.' })
-      console.log('user added successfully')
-    })
-  }
-// *********************************************
-
 // Creates multiple methods for the same route
 app
   .route('/v1/user')
   // GET endpoint
-  .get(getUser)
+  .get(controllers.getUser)
   // POST endpoint
-  .post(addUser)
+  .post(controllers.addUser)
+  // PUT endpoint
+  .put(controllers.updateUser)
 
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
