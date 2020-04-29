9666d1c860a2b6152446c6977bf5e99b675045ca
Alejandro <morsisdivine@gmail.com>
2020-04-29 22:10:03 +1000

    run pointing to perf DB

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require('body-parser')
-// const { pool } = require('./config')
 const cors = require('cors')
 let controllers = require('./controller/sql');
 
@@ -20,30 +19,6 @@ app.use(cors())
 
 // The below routes are for swagger documentation
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// TODO: move the below into a separate V1 controller *********************************************
-// const getUser = (request, response) => {
-//     pool.query('SELECT * FROM leaderboard ORDER BY score DESC', (error, results) => {
-//       if (error) {
-//         throw error
-//       }
-//       response.status(200).json(results.rows)
-//       console.log('user list loaded successfully')
-//     })
-//   }
-  
-//   const addUser = (request, response) => {
-//     const { username, score } = request.body
-  
-//     pool.query('INSERT INTO leaderboard (username, score) VALUES ($1, $2)', [username, score], error => {
-//       if (error) {
-//         throw error
-//       }
-//       response.status(201).json({ status: 'success', message: 'User added.' })
-//       console.log('user added successfully')
-//     })
-//   }
-// *********************************************
 
 // Creates multiple methods for the same route
 app
