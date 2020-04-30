873f7675b0325b83daeafea396a70f59df98457c
Alejandro <morsisdivine@gmail.com>
2020-04-30 22:41:22 +1000

    error handling (#8)
    * error handling
    
    * log in console for trouble shooting
    
    * fix test

--- app/controller/sql.js
+++ app/controller/sql.js
@@ -3,22 +3,28 @@ const { pool } = require('../config')
 const getUser = (request, response) => {
     pool.query('SELECT * FROM leaderboard ORDER BY score DESC', (error, results) => {
       if (error) {
-        throw error
+        console.log(error.stack)
+        response.status(400)
+        response.json({error: error.stack})
+        } else {
+        response.status(200).json(results.rows)
+        console.log('user list loaded successfully')
       }
-      response.status(200).json(results.rows)
-      console.log('user list loaded successfully')
     })
   }
   
-const addUser = (request, response) => {
+const addUser = (request, response, next) => {
     const { username, score } = request.body
 
     pool.query('INSERT INTO leaderboard (username, score) VALUES ($1, $2)', [username, score], error => {
         if (error) {
-        throw error
+          console.log(error.stack)
+          response.status(400)
+          response.json({error: error.stack})
+          } else {
+          response.status(201).json({ status: 'success', message: 'User added.' })
+          console.log('user added successfully')
         }
-        response.status(201).json({ status: 'success', message: 'User added.' })
-        console.log('user added successfully')
     })
 }
 
@@ -27,10 +33,13 @@ const updateUser = (request, response) => {
 
     pool.query('UPDATE leaderboard SET score = $2 WHERE username = $1', [username, score], error => {
         if (error) {
-        throw error
+          console.log(error.stack)
+          response.status(400)
+          response.json({error: error.stack})
+        } else {
+          response.status(204).json({ status: 'success', message: 'User updates.' })
+          console.log('user updated successfully')
         }
-        response.status(204).json({ status: 'success', message: 'User updates.' })
-        console.log('user updated successfully')
     })
 }
 
