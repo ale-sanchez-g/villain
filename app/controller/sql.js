40757779f09e555cfe5dca37fd0223c352c1feab
Alejandro <morsisdivine@gmail.com>
2021-09-07 16:15:14 +1000

    local test and auth on the API

--- app/controller/sql.js
+++ app/controller/sql.js
@@ -1,66 +1,117 @@
-const { pool } = require('../config')
+const jwt = require("jsonwebtoken");
+const tokenKey = "secret" || process.env.AUTH_KEY;
+
+const { pool } = require("../config");
 
 const getUser = (request, response) => {
-    pool.query('SELECT * FROM leaderboard ORDER BY score DESC', (error, results) => {
-      if (error) {
-        console.log(error.stack)
-        response.status(400)
-        response.json({error: error.stack})
-        } else {
-        response.status(200).json(results.rows)
-        console.log('user list loaded successfully')
-      }
-    })
-  }
-  
-const addUser = (request, response) => {
-    const { username, score } = request.body
-
-    pool.query('INSERT INTO leaderboard (username, score) VALUES ($1, $2)', [username, score], error => {
-        if (error) {
-          console.log(error.stack)
-          response.status(400)
-          response.json({error: error.stack})
+  authenticate(request, response, error => {
+    if (!error) {
+      pool.query(
+        "SELECT * FROM leaderboard ORDER BY score DESC",
+        (error, results) => {
+          if (error) {
+            console.log(error.stack);
+            response.status(400);
+            response.json({ error: error.stack });
           } else {
-          response.status(201).json({ status: 'success', message: 'User added.' })
-          console.log('user added successfully')
+            response.status(200).json(results.rows);
+            console.log("user list loaded successfully");
+          }
         }
-    })
-  }
+      );
+    }
+  });
+};
+
+const addUser = (request, response) => {
+  const { username, score } = request.body;
+  authenticate(request, response, error => {
+    if (!error) {
+      pool.query(
+        "INSERT INTO leaderboard (username, score) VALUES ($1, $2)",
+        [username, score],
+        (error) => {
+          if (error) {
+            console.log(error.stack);
+            response.status(400);
+            response.json({ error: error.stack });
+          } else {
+            response
+              .status(201)
+              .json({ status: "success", message: "User added." });
+            console.log("user added successfully");
+          }
+        }
+      );
+    };
+  });
+};
 
 const updateUser = (request, response) => {
-    const { username, score } = request.body
-
-    pool.query('UPDATE leaderboard SET score = $2 WHERE username = $1', [username, score], (error, result) => {
-        if (result.rowCount == 0) {
-            pool.query('INSERT INTO leaderboard (username, score) VALUES ($1, $2)', [username, score], error => {
-            response.status(201).json({ status: 'success', message: 'User added with updated score' })
-          })
-        } else {
-          response.status(204)
-          response.json()
-          console.log('user updated successfully')
+  const { username, score } = request.body;
+  authenticate(request, response, error => {
+    if (!error) {
+      pool.query(
+        "UPDATE leaderboard SET score = $2 WHERE username = $1",
+        [username, score],
+        (error, result) => {
+          if (result.rowCount == 0) {
+            pool.query(
+              "INSERT INTO leaderboard (username, score) VALUES ($1, $2)",
+              [username, score],
+              (error) => {
+                response
+                  .status(201)
+                  .json({
+                    status: "success",
+                    message: "User added with updated score",
+                  });
+              }
+            );
+          } else {
+            response.status(204);
+            response.json();
+            console.log("user updated successfully");
+          }
         }
-    })
-  }
+      );
+    };
+  });
+};
 
 const cleanUsers = (request, response) => {
   if (request.header("delete-key") == process.env.DEL_KEY) {
-    pool.query('DELETE FROM leaderboard WHERE Score IS NOT NULL', (error, results) => {
-      if (error) {
-          console.log(error.stack)
-          response.status(400)
-          response.json({error: error.stack})
+    pool.query(
+      "DELETE FROM leaderboard WHERE Score IS NOT NULL",
+      (error, results) => {
+        if (error) {
+          console.log(error.stack);
+          response.status(400);
+          response.json({ error: error.stack });
         } else {
-          response.status(204).json(results.rows)
-          console.log('user list cleaned successfullsy')
+          response.status(204).json(results.rows);
+          console.log("user list cleaned successfullsy");
+        }
       }
-    });
+    );
   } else {
-    console.log("ERR401-unable-to-authenticate-delete-key")
-    response.status(401)
-    response.json({error: "unable to authenticate key"})
+    console.log("ERR401-unable-to-authenticate-delete-key");
+    response.status(401);
+    response.json({ error: "unable to authenticate key" });
   }
-  }
-  
-module.exports = { getUser, addUser, updateUser, cleanUsers }
+};
+
+function authenticate(request, response, next) {
+  const token = request.headers.authorization;
+  jwt.verify(token, tokenKey, (error, decoded) => {
+    if (error) {
+      response
+        .status(403)
+        .json({ error: "Token Authentication failed ::: " + error });
+    } else {
+      console.log("Application used ::: " + decoded.key);
+      next();
+    }
+  });
+}
+module.exports = { getUser, addUser, updateUser, cleanUsers };
