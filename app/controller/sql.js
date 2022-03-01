f2e87ec1defdb9b517a46147ac71c35de061c4b2
Alejandro <morsisdivine@gmail.com>
2022-03-01 12:36:41 +1100

    fix for invalid data and test

--- app/controller/sql.js
+++ app/controller/sql.js
@@ -26,23 +26,31 @@ const getUser = (request, response) => {
 const addUser = (request, response) => {
   const { username, score } = request.body;
   authenticate(request, response, error => {
-    if (!error) {
-      pool.query(
-        "INSERT INTO leaderboard (username, score) VALUES ($1, $2)",
-        [username, score],
-        (error) => {
-          if (error) {
-            console.log(error.stack);
-            response.status(400);
-            response.json({ error: error.stack });
-          } else {
-            response
-              .status(201)
-              .json({ status: "success", message: "User added." });
-            console.log(`user ${username} added successfully`);
+    var err = validateScore(score);
+    if (err!== undefined) {
+      console.log(err);
+      response.status(400);
+      response.json({ error: err });
+    }  
+    else {
+      if (!error) {
+        pool.query(
+          "INSERT INTO leaderboard (username, score) VALUES ($1, $2)",
+          [username, score],
+          (error) => {
+            if (error) {
+              console.log(error.stack);
+              response.status(400);
+              response.json({ error: error.stack });
+            } else {
+              response
+                .status(201)
+                .json({ status: "success", message: "User added." });
+              console.log(`user ${username} added successfully`);
+            }
           }
-        }
-      );
+        );
+      }
     };
   });
 };
@@ -50,32 +58,40 @@ const addUser = (request, response) => {
 const updateUser = (request, response) => {
   const { username, score } = request.body;
   authenticate(request, response, error => {
-    if (!error) {
-      pool.query(
-        "UPDATE leaderboard SET score = $2 WHERE username = $1",
-        [username, score],
-        (error, result) => {
-          if (result.rowCount == 0) {
-            pool.query(
-              "INSERT INTO leaderboard (username, score) VALUES ($1, $2)",
-              [username, score],
-              (error) => {
-                response
-                  .status(201)
-                  .json({
-                    status: "success",
-                    message: `User ${username} added with updated score`,
-                  });
-                console.log(`user ${username} added with updated score`);
-              }
-            );
-          } else {
-            response.status(204);
-            response.json();
-            console.log(`user ${username} updated successfully`);
+    var err = validateScore(score);
+    if (err !== undefined) {
+      console.log(err);
+      response.status(400);
+      response.json({ error: err });
+    }  
+    else {
+      if (!error) {
+        pool.query(
+          "UPDATE leaderboard SET score = $2 WHERE username = $1",
+          [username, score],
+          (error, result) => {
+            if (result.rowCount == 0) {
+              pool.query(
+                "INSERT INTO leaderboard (username, score) VALUES ($1, $2)",
+                [username, score],
+                (error) => {
+                  response
+                    .status(201)
+                    .json({
+                      status: "success",
+                      message: `User ${username} added with updated score`,
+                    });
+                  console.log(`user ${username} added with updated score`);
+                }
+              );
+            } else {
+              response.status(204);
+              response.json();
+              console.log(`user ${username} updated successfully`);
+            }
           }
-        }
-      );
+        );
+      }
     };
   });
 };
@@ -134,4 +150,9 @@ function authenticateUser(request, response, next) {
   });
 }
 
+function validateScore (score) {
+  if ( /[^a-zA-Z0-9\-\/]/.test( score )  || score.length === 0) {
+    return`ERR400-invalid-score-value`;
+  }
+}
 module.exports = { getUser, addUser, updateUser, cleanUsers };
