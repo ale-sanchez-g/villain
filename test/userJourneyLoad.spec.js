ab9d4a731f288a9c133fe9fbfd77505d597137e9
Alejadro Sanchez-Giraldo <morsisdivine@gmail.com>
2023-08-24 22:41:19 +1000

    run soak test

--- test/userJourneyLoad.spec.js
+++ test/userJourneyLoad.spec.js
@@ -1,54 +1,54 @@
-import { sleep } from 'k6';
+import { sleep } from "k6";
 import { group, check } from "k6";
 import http from "k6/http";
 import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.1.0/index.js";
 
 export let options = {
   stages: [
-    { duration: '1m', target: 1},
-    { duration: '5m', target: 15 },
-    { duration: '5m', target: 15 },
-    { duration: '1m', target: 0 },
+    { duration: "1m", target: 1 },
+    { duration: "5m", target: 15 },
+    { duration: "60m", target: 15 },
+    { duration: "1m", target: 0 },
   ],
   thresholds: {
-    http_req_failed: ['rate<0.01'],   // http errors should be less than 1% 
-    http_req_duration: ['p(95)<1700'], // 95% of requests should be below 1.7 seconds
+    http_req_failed: ["rate<0.01"], // http errors should be less than 1%
+    http_req_duration: ["p(95)<1700"], // 95% of requests should be below 1.7 seconds
   },
 };
 
-export default function() {
-let token = getToken();
-let params = {
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': token,
-  },
-};
+export default function () {
+  let token = getToken();
+  let params = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+  };
 
-  group('villain test', function() {
+  group("villain test", function () {
     let r = Math.random().toString(36).substring(7);
 
     // Long user journey
-    group('Create user', function() {
+    group("Create user", function () {
       let payload = JSON.stringify({
         username: r,
         score: 0,
       });
 
-      let cures = http.post(`${__ENV.MY_HOSTNAME}/v1/user`,payload, params);
+      let cures = http.post(`${__ENV.MY_HOSTNAME}/v1/user`, payload, params);
 
       check(cures, {
-        "Create user status 201": r => r.status === 201,
+        "Create user status 201": (r) => r.status === 201,
       });
 
       var times = 10;
-      for(var i=0; i < times; i++){
+      for (var i = 0; i < times; i++) {
         sleep(randomIntBetween(10, 20));
 
-        let uures = http.put(`${__ENV.MY_HOSTNAME}/v1/user`,payload, params);
+        let uures = http.put(`${__ENV.MY_HOSTNAME}/v1/user`, payload, params);
 
         check(uures, {
-          "Update user status 204": r => r.status === 204,
+          "Update user status 204": (r) => r.status === 204,
         });
       }
 
@@ -57,18 +57,16 @@ let params = {
       let gures = http.get(`${__ENV.MY_HOSTNAME}/v1/user`, params);
 
       check(gures, {
-        "Get list of users status 200": r => r.status === 200,
+        "Get list of users status 200": (r) => r.status === 200,
       });
-
     });
-
   });
-};
+}
 
-function getToken () {
+function getToken() {
   let params = {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   };
   let r = Math.random().toString(36).substring(7);
@@ -76,12 +74,16 @@ function getToken () {
     key: r,
     email: `${r}@yopmail.com`,
     returnKey: true,
-    expiresIn: "2 days"
+    expiresIn: "2 days",
   });
-  let authres = http.post(`${__ENV.MY_HOSTNAME}/auth/gentoken`,payload, params);
+  let authres = http.post(
+    `${__ENV.MY_HOSTNAME}/auth/gentoken`,
+    payload,
+    params
+  );
   let token = authres.json().token;
   check(authres, {
-    "Application added status 200": r => r.status === 200,
+    "Application added status 200": (r) => r.status === 200,
   });
   return token;
 }
