dfe0873684475f5e6ee4ae82536319727e04823a
Alejadro Sanchez-Giraldo <morsisdivine@gmail.com>
2023-09-27 13:37:15 +1000

    added cy test

--- cypress/integration/registration/auth.app.spec.js
+++ cypress/integration/registration/auth.app.spec.js
@@ -1,9 +1,42 @@
-describe('TODO api testing', () => {
-    let todoItem;
-    it('fetches Todo items - GET', () => {
-        cy.request('/api-docs/').as('swaggerDocs');
-        cy.get('@swaggerDocs').then(swgdoc => {
-            expect(swgdoc.status).to.eq(200);
-        });
+const faker = require("faker");
+
+describe("API tests", () => {
+  let authToken;
+
+  it("should create an account and get auth token", () => {
+    const email = faker.internet.email();
+    const key = faker.internet.password();
+
+    cy.request({
+      method: "POST",
+      url: "/auth/gentoken",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        key: key,
+        email: email,
+        returnKey: true,
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property("token");
+      authToken = response.body.token;
     });
- });
+  });
+
+  it("should use auth token to get user data", () => {
+    cy.wrap(authToken).then((token) => {
+      cy.request({
+        method: "GET",
+        url: "/auth/verifytoken",
+        headers: {
+          Authorization: `${token}`,
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body[0]).to.have.property("app_id");
+      });
+    });
+  });
+});
